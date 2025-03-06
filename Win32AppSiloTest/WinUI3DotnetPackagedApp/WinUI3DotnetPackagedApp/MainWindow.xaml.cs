4b8c06ab01c5d008e5e97782f251e068f12bd857
Xiang Hong <hx.hongxiang@gmail.com>
2025-03-06 15:25:32 +0800

    init commit for a quick test demo

---
+++ Win32AppSiloTest/WinUI3DotnetPackagedApp/WinUI3DotnetPackagedApp/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Windows.Storage;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace WinUI3DotnetPackagedApp
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void ReadUsingFileIO(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var path = filePath.Text;
+                if (string.IsNullOrEmpty(path))
+                {
+                    return;
+                }
+                var file = await StorageFile.GetFileFromPathAsync(path);
+                var content = await FileIO.ReadTextAsync(file);
+                outputContent.Text = content;
+            }
+            catch (Exception err)
+            {
+                outputContent.Text = err.Message;
+            }
+        }
+
+        private void ReadUsingDotnetAPI(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var path = filePath.Text;
+                var content = File.ReadAllText(path);
+                outputContent.Text = content;
+            }
+            catch (Exception err)
+            {
+                outputContent.Text = err.Message;
+            }
+        }
+
+        private async void ClearContent(object sender, RoutedEventArgs e)
+        {
+            outputContent.Text = "";
+        }
+
+        private async void ReadUsingFileDialog(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var path = await Win32ApiUtilitiy.PickFileViaDialog();
+                filePath.Text = path;
+                var content = File.ReadAllText(path);
+                outputContent.Text = content;
+            }
+            catch (Exception err)
+            {
+                outputContent.Text = err.Message;
+            }
+
+        }
+    }
+}
---
+++ Win32AppSiloTest/WinUI3DotnetPackagedApp/WinUI3DotnetPackagedApp/Win32ApiUtilitiy.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Win32;
+using Windows.Win32.Foundation;
+using Windows.Win32.System.Com;
+using Windows.Win32.UI.Shell;
+using Windows.Win32;
+using Windows.Win32.Security;
+
+namespace WinUI3DotnetPackagedApp;
+
+static class Win32ApiUtilitiy
+{
+    public static unsafe async Task<string> PickFileViaDialog()
+    {
+        PInvoke.CoCreateInstance(typeof(FileOpenDialog).GUID, null, CLSCTX.CLSCTX_INPROC_SERVER, out IFileOpenDialog picker).ThrowOnFailure();
+        //if (GetTokenIsAppContainer())
+        //{
+        //    picker.SetOptions(FILEOPENDIALOGOPTIONS.FOS_HIDEMRUPLACES | FILEOPENDIALOGOPTIONS.FOS_HIDEPINNEDPLACES);
+        //    var folder = Windows.ApplicationModel.Package.Current.InstalledLocation;
+        //    IShellItem initialFolder = ComFolderPickerCommand.GetShellItemForPath(folder.Path);
+        //    if (initialFolder != null)
+        //    {
+        //        picker.SetFolder(initialFolder);
+        //        picker.SetDefaultFolder(initialFolder);
+        //    }
+        //}
+        picker.Show(default);
+        picker.GetResult(out var item);
+        item.GetDisplayName(SIGDN.SIGDN_FILESYSPATH, out var pathStr);
+        var result = pathStr.ToString();
+        Marshal.FreeCoTaskMem((nint)pathStr.Value);
+        return result;
+    }
+    public unsafe static bool GetTokenIsElevated()
+    {
+        // from https://learn.microsoft.com/en-us/windows/win32/secauthz/appcontainer-for-legacy-applications-#c-pinvoke
+        UInt32 TOKEN_QUERY = 0x0008;
+
+        var ph = PInvoke.GetCurrentProcess();
+        var safeHandle = new SafeProcessHandle((nint)ph.Value, ownsHandle: false);
+        if (!PInvoke.OpenProcessToken(
+            safeHandle,
+            TOKEN_ACCESS_MASK.TOKEN_QUERY,
+            out var tokenHandle))
+        {
+            throw new Exception("Win32 API Exception");
+            // Handle the error.
+        }
+
+        uint result;
+        uint TokenIsAppContainer = 29;
+        uint tokenInformationLength = sizeof(uint);
+
+        if (!PInvoke.GetTokenInformation(
+            tokenHandle,
+            TOKEN_INFORMATION_CLASS.TokenElevation,
+            &result,
+            tokenInformationLength,
+            out tokenInformationLength))
+        {
+            throw new Exception("Win32 API Exception");
+            // Handle the error.
+        }
+
+        return result != 0;
+    }
+    public unsafe static bool GetTokenIsAppContainer()
+    {
+        // from https://learn.microsoft.com/en-us/windows/win32/secauthz/appcontainer-for-legacy-applications-#c-pinvoke
+        UInt32 TOKEN_QUERY = 0x0008;
+
+        var ph = PInvoke.GetCurrentProcess();
+        var safeHandle = new SafeProcessHandle((nint)ph.Value, ownsHandle: false);
+        if (!PInvoke.OpenProcessToken(
+            safeHandle,
+            TOKEN_ACCESS_MASK.TOKEN_QUERY,
+            out var tokenHandle))
+        {
+            throw new Exception("Win32 API Exception");
+            // Handle the error.
+        }
+
+        uint isAppContainer;
+        uint TokenIsAppContainer = 29;
+        uint tokenInformationLength = sizeof(uint);
+
+        if (!PInvoke.GetTokenInformation(
+            tokenHandle,
+            TOKEN_INFORMATION_CLASS.TokenIsAppContainer,
+            &isAppContainer,
+            tokenInformationLength,
+            out tokenInformationLength))
+        {
+            throw new Exception("Win32 API Exception");
+            // Handle the error.
+        }
+
+        return isAppContainer != 0;
+    }
+}
